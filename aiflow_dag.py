b5631f51d42af13b1c0212573e7a02e425a132cd
EbubeDelight <60972198+EbubeDelight@users.noreply.github.com>
2024-05-27 03:05:19 +0200

    made some changes to the dag function

--- aiflow_dag.py
+++ aiflow_dag.py
@@ -3,36 +3,53 @@ from airflow import DAG
 from airflow.decorators import dag, task
 from airflow.operators.python import PythonOperator
 from datetime import datetime
-from spark import extract_data
+# from spark import extract_data
 from spark import transform_data
 from spark import load_data
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
 # Define DAG parameters
 dag = DAG(
     dag_id='uber_etl',
     schedule_interval='@daily',  # Schedule the pipeline to run daily
     start_date=datetime(2024, 5, 23),
-    catchup = False,
+    catchup=False,
+)
+start = PythonOperator(
+    task_id='start',
+    python_callable= lambda: print("uber_etl_started"),
+    dag=dag,
 )
 
-    
-extract_task = PythonOperator(
-        task_id='extract_data',
-        python_callable=extract_data,
-        dag=dag,
-        )
-    
-transform_task = PythonOperator(
-        task_id='transform_data',
-        python_callable=transform_data,
-        dag=dag,
-        )
-    
-load_task = PythonOperator(
-        task_id='load_data_to_bigquery',
-        python_callable=load_data,
-        dag=dag,
-        )
+spark_job = SparkSubmitOperator(
+    task_id="spark_job",
+    conn_id="spark-conn",
+    application="spark.py",
+    dag=dag
+)
+
+end = PythonOperator(
+    task_id='end',
+    python_callable= lambda: print("uber_etl_ended"),
+    dag=dag,
+)
 
 # Define task dependencies
-extract_task >> transform_task >> load_task
+start >> spark_job >> end
+# extract_task = PythonOperator(
+# task_id='extract_data',
+# python_callable=extract_data,
+# dag=dag,
+# )
+
+#transform_task = PythonOperator(
+ #   task_id='transform_data',
+   # python_callable=transform_data,
+  #  dag=dag,
+#)
+
+#load_task = PythonOperator(
+ #   task_id='load_data_to_bigquery',
+  #  python_callable=load_data,
+   # dag=dag,
+#)
